1e67931e106968fa42a401271892643084ea4dcc
You-jisang <dbwltkd1019@naver.com>
2023-10-23 22:37:51 +0900

    feat: 객프 테스트 레포 생성

---
+++ app/src/main/java/com/example/mymultifragapplication/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.mymultifragapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.mymultifragapplication.databinding.ActivityMainBinding
+
+val String.numOfkoreanCharacters: Int
+    get() {
+        var count = 0
+        for (i in 0 until length) {
+            if (this[i] >= 0xAC00.toChar() && this[i] <= 0xD7A3.toChar()) {
+                count += 1
+            }
+        }
+
+        return count
+    }
+
+class MainActivity : AppCompatActivity() {
+    var text: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        fun replaceFragment(frag: Fragment) {
+            supportFragmentManager.beginTransaction().run {
+                replace(binding.frmFrag.id, frag)
+                commit()
+            }
+        }
+
+
+
+        binding.run {
+            btnInput.setOnClickListener {
+                replaceFragment(InputFragment.newInstance())
+            }
+            btnResult.setOnClickListener {
+                replaceFragment(ResultFragment.newInstance(text?.numOfkoreanCharacters ?: 0))
+            }
+        }
+
+
+        supportFragmentManager.setFragmentResultListener("input_text", this) { _, bundle ->
+            text = bundle.getString("input")
+        }
+        replaceFragment(InputFragment.newInstance())
+
+
+
+        setContentView(binding.root)
+    }
+}
